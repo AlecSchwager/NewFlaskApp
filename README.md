70ec464c7261e2d3c47aec903de83f56b4cbc513
Alec Schwager <alec-schwager@uiowa.edu>
2022-04-24 15:41:15 -0500

    Copied Old Code From VTM Site
    Hoping to fix commit issue

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Flask Demo
+# Project Name
+*Flask Application Demo*
+
 ![MicrosoftAzure](https://img.shields.io/badge/-Azure-blue?logo=microsoftazure)
-![Flask](https://img.shields.io/badge/-Flask-black?logo=flask)
+![Flask](https://img.shields.io/badge/-Flask-black?logo=Flask)
+![Python](https://img.shields.io/badge/-Python-yellow?logo=Python)
+# What Did I Learn?
+*This project taught me how to use Flask and create Flask apps using Jinja templates, as well as reinforced my knowledge of Python.*
 
-Basic Flask application for class demonstration
 
----
+# Installation Instructions
 1. Clone this repository to local computer
 
-2. Create a new virtual environment. ```python -m venv ./venv```
+2. Create a new virtual environment 
+    - Windows:  ```python -m venv ./venv```
+    - Mac:  ```python3 -m venv ./venv```
 
 3. Activate the new virtual environment
    - Windows:  ```.\venv\Scripts\activate```
@@ -19,9 +25,10 @@ Basic Flask application for class demonstration
 
 6. Continue working with the project as you normally would.
 
-- [x] ~~Get Flask running on local computer~~
-- [ ] Add web form for users
+## To do
+
+- [x] Get Flask running on local computer
+- [x] Add web form for users
 - [ ] Collect web form data and manipulate
 - [ ] Read API data from another source
 - [ ] Read database data from a RDBMS
-
--- app.py
+++ app.py
@@ -3,28 +3,31 @@ from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-friend_list = [{"name": "Mike Colbert", "email":"mike@mike.com" } ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Mike\'s Friends', friends = friend_list)
+    return render_template('index.html', pageTitle='VTM Site')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About')
 
-@app.route('/add_friend', methods=['GET', 'POST'])
-def add_friend():
+@app.route('/estimate', methods=['GET','POST'])
+def estimate():
     if request.method == 'POST':
         form = request.form
-        fname = form['fname']
-        lname = form['lname']
-        email = form['email']
-        friend_dict = {"name": fname + " " + lname, "email": email}
-        friend_list.append(friend_dict)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
-    
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi = 3.14
+        topArea = pi * radius**2
+        sidesArea = 2*(pi*(radius*height))
+        totalArea = topArea+sidesArea
+        totalSqFt = totalArea / 144
+        materialCost = 25 * totalSqFt
+        laborCost = 15 * totalSqFt
+        totalCostEstimate = "${:,.2f}".format(round(materialCost + laborCost, 2))
+        return (render_template('estimate.html', estimate = totalCostEstimate))
+    return render_template('estimate.html', pageTitle='Estimator')
 
 if __name__ == '__main__':
     app.run(debug=True)
